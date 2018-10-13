150b3d62a927f356c5d2d3923b01c433cbc658a8
venglas <bartek.bober97@gmail.com>
2018-10-14 00:37:40 +0200

    add modal

--- washers/src/components/OurOffert.vue
+++ washers/src/components/OurOffert.vue
@@ -10,37 +10,53 @@
             </div>
         </div>
 
-        <div class="info-modal" v-for="modal in offert">
-            <h2>{{ modal.title }}</h2>
-            <h3>{{ modal.subtitle }}</h3>
-            <p>{{ modal.description }}</p>
-            <p>{{ modal.offertCaption }}</p>
+        <div class="info-modal" v-for="modal in offert" :id="'modal'+modal.id">
 
-            <u>
-                <li v-for="modalList in modal.offertList">
+            <img :src="closeIcon" alt="close icon" @click="closeModal(modal.id)" class="close-icon">
+            
+            <div class="section-top">
+                <h2>{{ modal.title }}</h2>
+                <h3>{{ modal.subtitle }}</h3>
+                <p>{{ modal.description }}</p>
+            </div>
+
+            <p class="offert-title">{{ modal.offertCaption }}</p>
+
+
+            <u class="info-modal-list">
+                <li v-for="modalList in modal.offertList" class="info-modal__item">
                     {{ modalList }}
                 </li>
             </u>
 
             <h3>W ramach zakupu usługi gwarantujemy chemię oraz sprzęt niezbędny do sprzątania.</h3>
 
-            <p>
-                <img :src="modal.iconTime" alt="calendar icon">
-                <p>{{ modal.time }}</p>
-            </p>
-
-            <p>
-                <img :src="modal.iconOrder" alt="order icon">
-                {{ modal.info1 }}
-            </p>
-
-            <div>
-                <p>cennik:</p> 
-                {{modal.price}}
+            <div class="info-item">
+                <img :src="modal.iconTime" alt="calendar icon" class="info-item__photo">
+                <p class="info-item__description">{{ modal.time }}</p>
             </div>
 
-            <ul>
-                <li v-for="modalList2 in modal.windowsClean">
+            <div class="info-item">
+                <img :src="modal.iconOrder" alt="order icon" class="info-item__photo">
+                <p class="info-item__description">{{ modal.info1 }}</p>
+            </div>
+
+            <div class="info-item">
+                <img :src="modal.iconContact" alt="contact icon" class="info-item__photo">
+                <p class="info-item__description">
+                    <p>{{ telephone }} / {{ email }}</p>
+                </p>
+            </div>
+
+            <div class="info-item">
+                <img :src="modal.iconPrice" alt="price icon" class="info-item__photo">
+                <p class="info-item__description">
+                    <p>{{modal.price}}</p>
+                </p>
+            </div>
+
+            <ul class="info-modal-list info-modal-list--2">
+                <li v-for="modalList2 in modal.windowsClean" class="info-modal__item">
                     {{modalList2}}
                 </li>
             </ul>
@@ -58,6 +74,7 @@ export default {
     return {
         telephone: '694-045-198',
         email: 'example@gmail.com',
+        closeIcon: '../../static/img/icons/close-icon.png',
         offert: [
             {
                 id: 1, img: '../../static/img/kaf1.png',
@@ -81,10 +98,13 @@ export default {
                 ],
                 iconTime: '../../static/img/icons/time-icon.png',
                 iconOrder: '../../static/img/icons/order-icon.png',
+                iconContact: '../../static/img/icons/contact-icon.png',
+                iconPrice: '../../static/img/icons/money-icon.png',
                 time: 'Biorąc pod uwagę komfort użytkowników biura, sprzątanie odbywa się po godzinach jego urzędowania, zgodnie z czasem zakontraktowanym i uzgodnionym z Klientem. Rozpoczęcie prac sprzątających odbywa się najwcześniej o godzinie 16.30.',
                 info1: 'Szybka realizacja zamówienia, po indywidualnym ustaleniu z Klientem jego oczekiwań oraz dogodnego terminu.',
                 price: 'godzina sprzątania 35zł',
                 windowsClean: [
+                    'Cennik mycia okien:',
                     'okno jednoskrzydłowe od 10 zł',
                     'okno dwuskrzydłowe od 15 zł',
                     'okno trzyskrzydłowe od 25 zł',
@@ -98,6 +118,15 @@ export default {
             // {id: 3, img: '../../static/img/kaf3.png', title: 'PRANIE TAPICERKI MEBLOWEJ', description: ''},
             // {id: 4, img: '../../static/img/kaf4.png', title: 'WYPOŻYCZALNIA KARCHER', description: 'Urządzenie jest przeznaczone do prac porządkowych w miejscach, gdzie do zapewnienia czystości potrzebne są wytrzymałe i uniwersalne urządzenia. Niesprzyjające warunki i duże obciążenie panujące podczas prac porządkowych w transporcie, motoryzacji, budownictwie, rolnictwie czy pracy rzemieślniczej nie stanowią żadnej przeszkody dla tego odkurzacza.'},
         ]
+    }
+  },
+  methods: {
+    closeModal(which){
+        switch(which){
+            case 1:
+                $('#modal'+which).css({display: 'none'})
+            break;
+        }
     }
   }
 }
@@ -187,6 +216,47 @@ export default {
     left: 0;
     z-index: 1999;
     padding: 5rem;
+    color: #fff;
+    font-size: 1.5rem;
+
+    .close-icon{
+        position: absolute;
+        top: 2rem;
+        right: 5rem;
+        cursor: pointer;
+    }
+    .section-top{
+        text-align: center;
+        font-size: 1.5rem;
+        *{
+            padding: 1rem 0;
+        }
+    }
+    .offert-title{
+        font-size: 1.7rem;
+        padding: 2rem 0 1rem 0;
+    }
+    .info-modal-list{
+        margin-left: 2rem;
+        text-decoration: none;
+        font-size: 1.5rem;
+        &--2{
+            li{
+                &:first-child{
+                    list-style: none;
+                    font-weight: 600;
+                    margin-left: -2rem;
+                }
+            }
+        }
+    }
+    .info-item{
+        padding: 1.5rem 0;
+        display: flex;
+        &__photo{
+            margin-right: 1rem;
+        }
+    }
 }
 
 </style>
